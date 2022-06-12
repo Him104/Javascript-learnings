ef2ec867af97402c679d5a1d350d26e9cfaa2e4d
Him104 <himanshuchauhan696@gmail.com>
2022-06-12 18:42:10 +0530

    adding javascript

---
+++ index.js
@@ -0,0 +1,83 @@
+// console.log("hello world!!!");
+
+// let a;
+
+// console.log(a);
+
+// -----------------------------------------------
+//string Indexing
+
+// let Name = "himanshu";
+
+// // console.log(Name[0]);
+
+// console.log(Name.length) 
+
+// console.log(Name[Name.length-1])
+
+// ------------------------------------------------
+
+//trim()
+//toUpperCase()
+//toLoweCase()
+//slice
+
+// let firstName = "  Mukesh   ";
+
+// console.log(firstName.length);
+// let newString = firstName.trim()
+
+// console.log(newString.length)
+
+// let firstName = "Mukesh";
+
+// let newString = firstName.toUpperCase()
+
+// console.log(newString)
+
+// let firstName = "MUKESH";
+
+// let newString = firstName.toLowerCase()
+
+// console.log(newString)
+
+// start index
+
+// end index
+
+// let firstName = "MUKESH";
+
+// let newString = firstName.slice(0,1)
+// console.log(newString)
+
+//--------------------------------------
+//typeof operator
+//data types (primitive data types) :-
+
+//string
+//number
+//booleans
+//undefined
+//null
+// BigInt
+//symbol
+
+// convert number to string
+// convert string to number
+
+// let age = 29;
+// let firstName = "Himanshu Chauhan";
+
+// console.log(typeof(age))
+
+// // convert number to string
+// let age = 34;
+// age = 29 + "";
+
+// console.log(typeof age)
+
+// convert number to string
+let age = 34;
+age = 29 + "";
+
+console.log(typeof age)
