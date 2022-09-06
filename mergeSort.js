27738928cecf963d582c51da92b69b3553568c2d
Him104 <himanshuchauhan696@gmail.com>
2022-09-06 13:22:05 +0530

    -----------------------

--- mergeSort.js
+++ mergeSort.js
@@ -30,6 +30,7 @@ function mergeSort(arr){
    let mid = Math.floor(arr.length/2);
    let left = mergeSort(arr.slice(0,mid));
    let right = mergeSort(arr.slice(mid));
+   console.log(left,right);
 
     return merge(left,right);
 }
---
+++ quickSort.js
@@ -0,0 +1,6 @@
+function quick(arr){
+    return arr;
+}
+
+let arr = [10,2,8,6,4];
+console.log(quickSort(arr));
