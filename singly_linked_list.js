9f245fce28ccd0e28fca3034a802aa9426ffb874
Him104 <himanshuchauhan696@gmail.com>
2022-09-07 15:40:11 +0530

    adding linklist

--- singly_linked_list.js
+++ singly_linked_list.js
@@ -7,8 +7,39 @@ class linkedList {
             this.tail = this.head     
     
 }
+
+insert(data){
+    const secondNode = {
+        value: data,
+        next: null
+    }
+    this.tail.next = secondNode
+    this.tail = secondNode;
+
+
+}
+insert(data){
+    const thirdNode = {
+        value:data,
+        next:null
+    }
+    this.tail.next = thirdNode
+    this.tail = thirdNode
+}
+
+insert(data){
+    const fourthNode = {
+        value:data,
+        next:null
+    }
+    this.tail.next = fourthNode
+    this.tail = fourthNode
+}
 }
 
 const myList = new linkedList(10)
+myList.insert(16)
+myList.insert(29)
+myList.insert(33)
 
 console.log(myList);
