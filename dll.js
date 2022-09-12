8b64aefaadb504b1a164c47e207533fc18e99b5e
Him104 <himanshuchauhan696@gmail.com>
2022-09-12 16:15:52 +0530

    adding dll

--- dll.js
+++ dll.js
@@ -1,12 +1,12 @@
 class Node{
     constructor(data){
-        this.data = val;
+        this.data = data;
         this.next = null;
         this.prev = null;
     }
 }
 
-class DoublyLinkedList {
+class dll {
     constructor(){
         this.head = null;
         this.tail = null;
@@ -25,9 +25,96 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+
+    pop(){
+        if(!this.head) return undefined;
+        let poppedNode = this.tail;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+        this.length--;
+        return poppedNode;
+    
+}
+shift(){
+    if(this.length === 0) return undefined;
+    var oldHead = this.head;
+    if(this.length === 1){
+        this.head = null;
+        this.tail = null;
+    }else{
+        this.head = oldHead.next;
+        this.head.prev = null;
+        oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+}
+unshift(data){
+    var newNode = new Node(data);
+    if(this.length === 0) {
+        this.head = newNode;
+        this.tail = newNode;
+    } else {
+        this.head.prev = newNode;
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+    this.length++;
+    return this;
+}
+get(index){
+    if(index < 0 || index >= this.length) return null;
+    var count, current;
+    if(index <= this.length/2){
+        count = 0;
+        current = this.head;
+        while(count !== index){
+            current = current.next;
+            count++;
+        }
+    } else {
+        count = this.length - 1;
+        current = this.tail;
+        while(count !== index){
+            current = current.prev;
+            count--;
+        }
+    }
+    return current;
+}
+set(index, data){
+    var foundNode = this.get(index);
+    if(foundNode != null){
+        foundNode.data = data;
+        return true;
+    }
+    return false;
+}
+insert(index, data){
+    if(index < 0 || index > this.length) return false;
+    if(index === 0) return !!this.unshift(data);
+    if(index === this.length) return !!this.push(data);
+
+    var newNode = new Node(data);
+    var beforeNode = this.get(index-1);
+    var afterNode = beforeNode.next;
+    
+    beforeNode.next = newNode, newNode.prev = beforeNode;
+    newNode.next = afterNode, afterNode.prev = newNode;
+    this.length++;
+    return true;
+}
 }
 
-let list = DoublyLinkedList;
-list.push(1);
-
+let list = new dll()
+list.push(0);
+list.push(2);
+// list.pop()
+// list.shift()
 console.log(list);
