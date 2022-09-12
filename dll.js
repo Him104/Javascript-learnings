2707ce3624662f38c3f4de45eeb2fbe1a1949b8d
Him104 <himanshuchauhan696@gmail.com>
2022-09-12 13:18:58 +0530

    adding dll

---
+++ dll.js
@@ -0,0 +1,33 @@
+class Node{
+    constructor(data){
+        this.data = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(data){
+        var newNode = new Node(data);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+let list = DoublyLinkedList;
+list.push(1);
+
+console.log(list);
--- singly_linkedList2.js
+++ singly_linkedList2.js
@@ -74,6 +74,67 @@ get(index){
     }
     return current;
 }
+
+set(index,data){
+let foundNode = this.get(index)
+if(foundNode){
+    foundNode.data = data;
+    return true;
+}
+return false;
+
+}
+
+insert(index,data){
+    if(index<0 || index>this.length){
+        return false;
+    }
+    if(index=== this.length){
+        return this.push(data);
+    }
+    if (index===0) {
+        return this.unshift(data);
+        
+    }
+    let newNode = new Node(data);
+    let prev = this.get(index-1);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+}
+remove(index){
+    if(index<0 || index>this.length) {
+        return undefined;
+    }
+    if(index===0){
+        return this.shift(data);
+    }
+    if (index===this.length-1) {
+        return this.pop()
+         }
+    let previousNode = this.get(index-1);
+    let remove = previousNode.next;
+    previousNode.next = remove.next;
+    this.length--;
+    return remove;
+}
+reverse(){
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+        next = temp.next;
+        temp.next = prev;
+        prev = temp;
+        temp = next;
+    }
+    return this;
+}
+
 }
 let list = new linkedList()
 list.push("hey guys!!!")
@@ -81,6 +142,10 @@ list.push("goodbye!!")
 list.push("goodMorning!")
 // list.shift()
 // list.unshift("shifting to head")
-list.get(0)
+// list.get(0)
 // list.pop()
-// console.log(list)
+// list.set(0)
+// list.insert(1,"sayonara")
+// list.remove(1)
+list.reverse()
+console.log(list)
